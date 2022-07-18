d2d6f8551de09bbb8e40b97bbff5a4898f0f51ba
Anil Kumar <45123631+remoteservice1979@users.noreply.github.com>
2022-07-18 14:40:31 +0530

    Add files via upload
    Build file

---
+++ build/precache-manifest.fe7a02364024773e39e558a88ee8d822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1584d45f92259b924878",
+    "url": "/static/js/main.b55c977e.chunk.js"
+  },
+  {
+    "revision": "913f14d95317dd75307f",
+    "url": "/static/js/2.7eb2ea2c.chunk.js"
+  },
+  {
+    "revision": "1584d45f92259b924878",
+    "url": "/static/css/main.7037feb3.chunk.css"
+  },
+  {
+    "revision": "e127a9b4f968583ce643045fec6e6f8b",
+    "url": "/index.html"
+  }
+];
